c56c95f360c4f52a507968c6cc77287f55c37c27
Catherine Nguyen <catherinevn.11@gmail.com>
2021-02-04 19:49:57 -0500

    added kiosk

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -21,24 +21,53 @@ public class Kiosk {
 
             if (command.equals("A")) {
                 //do add
+                String newBookTitle = tokens[1];
+                Date newBookDatePublished = new Date(tokens[2]);
+                if (!newBookDatePublished.isValid()) {
+                    System.out.println("Invalid date!");
+                }
+                else {
+                    Book newBook = new Book("0", newBookTitle, false, newBookDatePublished);
+                    lib.add(newBook);
+                }
             }
             else if (command.equals("R")) {
                 //do remove
+                String serialNum = tokens[1];
             }
             else if (command.equals("O")) {
                 //do check out
+                String serialNum = tokens[1];
+                Book tempBook = new Book(tokens[1], "0", false, new Date());
+                if (lib.checkOut(tempBook)) {
+                    System.out.println("You've checked out Book#" + serialNum + ". Enjoy!");
+                }
+                else {
+                    System.out.println("Book#" + serialNum + " is not available.");
+                }
             }
             else if (command.equals("I")) {
                 //do return
+                String serialNum = tokens[1];
+                Book tempBook = new Book(tokens[1], "0", false, new Date());
+                if (lib.returns(tempBook)) {
+                    System.out.println("Book#" + serialNum + " return has completed. Thanks!");
+                }
+                else {
+                    System.out.println("Unable to return Book#" + serialNum + ".");
+                }
             }
             else if (command.equals("PA")) {
                 //do print all
+                lib.print();
             }
             else if (command.equals("PD")) {
                 //do print by dates
+                lib.printByDate();
             }
             else if (command.equals("PN")) {
                 //do print by numbers
+                lib.printByNumber();
             }
             else {
                 //invalid
